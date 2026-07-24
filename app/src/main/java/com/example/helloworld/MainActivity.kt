5d479b81d76489d35353c380749197139aecd42b
twaske2-dotcom <twaske2@gmail.com>
2026-07-24 22:03:14 +0530

    Hello World App

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            HelloScreen()
+        }
+    }
+}
+
+@Composable
+fun HelloScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello I am Tejas",
+            fontSize = 30.sp
+        )
+    }
+}
